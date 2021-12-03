8705721f3473aa04f1d99b30a78187fd176b91b0
A.N <spidernet12@gmail.com>
2021-12-03 14:08:56 +0300

    Create 1-datatypes.js

---
+++ 1-datatypes.js
@@ -0,0 +1,43 @@
+/* 
+Variables 
+Variables are similar to buckets, we place things/data inside of them so we can access it or modify it later.
+Variables can hold different type of data as well based on our needs.
+
+There are two ways of declaring variables in JavaScript:
+1- Using the var keyword
+2- using the let keyword
+Throughout this course, we will use let keyword exclusively.
+
+Constants
+Constants are almost identical to variables except that once we place something inside them, we cannot change its content.
+There are exception to this rule which we will discuss at a later stage.
+
+DataTypes
+
+*/
+
+// integers
+let age = 11;
+
+// floats
+let money = 11.500;
+
+// strings
+let name = "First Last";
+
+// boolean
+let isSubscribed = false;
+
+// arithmetic operations 
+
+const obj = {};
+
+obj.t = 1;
+
+console.log(obj);
+
+const a = [1,2];
+
+a.push(3);
+
+console.log(a);
