94db710dc9c9bf6a54dc8babad6b98b7d194917b
A.N <spidernet12@gmail.com>
2021-12-03 14:30:03 +0300

    Update 0-logging.js

--- 0-logging.js
+++ 0-logging.js
@@ -1,6 +1,9 @@
 /* To run this program, we need to do the following:
 1- Open the terminal in VS Code (CTRL + ` + t)
 2- type node 0-logging.js
+
+This file can be called a module. 
+A module is an encapsulated code that does not know anything about other modules/files unless we explicitly tell it so.
 */
 
 // Console Logging is one of the main ways of displaying program output to the user
