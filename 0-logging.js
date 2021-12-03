ea7075fe88b0353924560f330c3032345c8616e1
A.N <spidernet12@gmail.com>
2021-12-03 14:08:52 +0300

    Add Logging

---
+++ 0-logging.js
@@ -0,0 +1,7 @@
+/* To run this program, we need to do the following:
+1- Open the terminal in VS Code (CTRL + ` + t)
+2- type node 0-logging.js
+*/
+
+// Console Logging is one of the main ways of displaying program output to the user
+console.log("Hello World");
